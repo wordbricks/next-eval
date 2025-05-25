ad3fd46c81951eb58f935267541bb39deff9f8b9
maestroJeong <legend4020@snu.ac.kr>
2025-05-25 16:29:18 -0700

    feat: add originalHTML

--- WebDemo/src/lib/interfaces.ts
+++ WebDemo/src/lib/interfaces.ts
@@ -7,6 +7,7 @@ export interface HtmlResult {
   textMapLength: number;
   textMapFlat: Record<string, string>;
   textMapFlatLength: number;
+  originalHtml: string;
   originalHtmlLength?: number; // Optional as it might not always be available or relevant
   rawHtml?: string; // Added to store the original, unprocessed HTML
 } 
